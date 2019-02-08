dcb3e5f2b5403da66049d59e7cd2d60bece45f71
KBSHV <kibeshev889@gmail.com>
2019-02-08 16:25:16 +0300

    initial project

---
+++ kuda go/kuda go/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kuda go
+//
+//  Created by Кирилл Кибешев on 08.02.2019.
+//  Copyright © 2019 Кирилл Кибешев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
